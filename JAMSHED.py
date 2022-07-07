7454b7445c44d01749833eb50487c93030318719
Coodin-4Jamshedyaar <108851843+Coodin-4Jamshedyaar@users.noreply.github.com>
2022-07-07 10:05:41 +0500

    Create JAMSHED.py
    https://github.com/Coodin-4Jamshedyaar/JAMSHED-YAAR.git
    ls
    cd JAMSHEDYAAR
    ls
    python2  JAMSHED.py

---
+++ JAMSHED.py
@@ -0,0 +1,21 @@
+pkg update
+
+pkg upgrade
+
+pkg install git
+
+pkg install python
+
+pip install requests
+
+pip install mechanize
+
+pip install bs4
+
+rm -rf Random
+
+git clone https://github.com/AKING110/Random.git
+
+cd Random
+
+python AKING.py
